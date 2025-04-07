d32722e83fafdf04a36ad789def917cd0e49ef96
yifattih <yifattih@proton.me>
2025-04-07 19:57:33 +0000

    fix(service): add script to call the script that fetch identity tokens and updates the relevant nginx config

---
+++ service/scripts/update-nginx-config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+###############################################################################
+###############################################################################
+# Updates the authentication tokens
+###############################################################################
+###############################################################################
+source /scripts/fetch-token.sh
+# Function to update Nginx configuration with the latest token
+update_nginx_config() {
+    if [ -z ${API_TOKEN} ]; then
+        echo "Error: API TOKEN variable is not set."
+        exit 1
+    elif [ -z ${UI_TOKEN} ]; then
+        echo "Error: UI TOKEN variable is not set."
+        exit 1
+    fi
+
+    echo "Applying new token to Nginx configuration..."
+    envsubst '${API_TOKEN}' < /etc/nginx/conf.d/api-auth.conf.template > /etc/nginx/conf.d/api-auth.conf
+    envsubst '${UI_TOKEN}' < /etc/nginx/conf.d/ui-auth.conf.template > /etc/nginx/conf.d/ui-auth.conf
+
+    # Reload Nginx to apply changes
+    nginx -s reload
+    echo "Nginx reloaded successfully with the updated authentication token."
+}
+
+# Main execution
+update_nginx_config
